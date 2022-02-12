9c57954cb47a516714b8a2669ec9bc10d6837c7c
Luis G. Villegas <luisguveal@gmail.com>
2022-02-11 21:57:38 -0430

    Setting up CommentStats component

---
+++ src/components/CommentStats/CommentStats.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react"
+
+interface CommentStatsProps {
+  apiURL: string;
+  slug: string;
+}
+
+interface IStats {
+  count: number | null;
+}
+const defaultStatsState: IStats = {
+  count: null
+}
+
+const CommentStats = (props: CommentStatsProps) => {
+  const { slug, apiURL } = props
+  const [stats, setStats] = useState<IStats>(defaultStatsState)
+  useEffect(() => {
+    const fetchStats = async () => {
+      const url = `${apiURL}/api/comment-manager/comments/${slug}/count`
+      try {
+        const data_res = await fetch(url)
+        const data = await data_res.json()
+        if (!data_res.ok) {
+          throw data
+        }
+        setStats(data)
+      } catch(err) {
+        console.log("Error fetching comments stats")
+        console.log(err)
+        setStats({
+          count: 0
+        })
+      }
+    }
+    fetchStats()
+  }, [])
+  return (
+    <p style={{margin: 0}}>
+      {
+        (stats.count === null) ?
+          "loading comments"
+        : stats.count + (stats.count > 1 ? " comments" : " comment")
+      }
+    </p>
+  )
+}
+
+export default CommentStats
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { default as Comments } from "./Comments";
 export { default as ErrorBox } from "./ErrorBox";
 export { default as CommentForm, CommentFormProps } from "./CommentForm"
+export { default as CommentStats } from "./CommentStats"
 export {
   ConfigContext as CommentsConfigContext,
   IUser,
